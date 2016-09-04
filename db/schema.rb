ef79658dd9fbe25b98a43796e2070319c9f9c5eb
Ryan Kulp <ryanckulp@gmail.com>
2016-09-03 21:53:57 -0700

    cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160904023453) do
+ActiveRecord::Schema.define(version: 20160904044859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "playlists", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "soundcloud_id"
+    t.text     "track_ids"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "user_id"
+  end
+
+  add_index "playlists", ["user_id"], name: "index_playlists_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -37,4 +48,5 @@ ActiveRecord::Schema.define(version: 20160904023453) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "playlists", "users"
 end
